ad47592173f34cf7431ea240ff210714cf424d86
Joanne <90553540+joannemas@users.noreply.github.com>
2023-02-01 12:13:46 +0100

    ✨ Add home front

---
+++ barbapapa/src/Components/Home.js
@@ -0,0 +1,41 @@
+import React, {useEffect, useState} from 'react';
+import './Home.css';
+
+const Home = ({ recipes }) => {
+    return (
+        <div className='home'>
+            <h1>Welcome to Barbapapa !</h1>
+            <p>Find a recipe to your taste</p>
+
+
+            <h2>You may like these recipes :</h2>
+
+            <div className='recipes'>
+                <div className='recipe_box'>
+                    <h3>recipe title</h3>
+                    <img src={"https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Red_Apple.jpg/800px-Red_Apple.jpg"} />
+                    <a>View recipe</a>
+                </div>
+
+                <div className='recipe_box'>
+                    <h3>recipe title</h3>
+                    <img src={"https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Red_Apple.jpg/800px-Red_Apple.jpg"} />
+                    <a>View recipe</a>
+                </div>
+                <div className='recipe_box'>
+                    <h3>recipe title</h3>
+                    <img src={"https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Red_Apple.jpg/800px-Red_Apple.jpg"} />
+                    <a>View recipe</a>
+                </div>
+                <div className='recipe_box'>
+                    <h3>recipe title</h3>
+                    <img src={"https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Red_Apple.jpg/800px-Red_Apple.jpg"} />
+                    <a>View recipe</a>
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default Home;
