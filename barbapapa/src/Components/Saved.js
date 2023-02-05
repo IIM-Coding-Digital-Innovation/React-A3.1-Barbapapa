e23785cc491e9f50dc98ad40b4e08c7db61dd662
Joanne <90553540+joannemas@users.noreply.github.com>
2023-02-05 21:36:30 +0100

    💄 Add saved list style

--- barbapapa/src/Components/Saved.js
+++ barbapapa/src/Components/Saved.js
@@ -23,17 +23,18 @@ function Saved () {
 
 
     return (
-
         <div>
+            <h1>Saved Recipes</h1>
+            <div className='recipes'>
             {recipes.map(recipe => (
-                <div>
+                <div className='recipe_box'>
                     <img src={recipe.thumbnail_url} width="150" height="100" alt={recipe.name} />
-                    <p key={recipe.id}>{recipe.id} : {recipe.name} </p>
+                    <p key={recipe.id}>{recipe.name} </p>
                     <Link to={`/recipe/${recipe.id}`}>View recipe</Link>
                     <button onClick={() => handleRemoveRecipe(recipe.id)}>Remove</button>
-
                 </div>
             ))}
+            </div>
         </div>
     )
 
